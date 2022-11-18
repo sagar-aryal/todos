cab73724800bd3890909c7ded59b0fe6b86b450c
sagar-aryal <sagarandaryal@gmail.com>
2022-11-18 15:47:23 +0200

    responsive css fixed

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -4,44 +4,46 @@ const Table = ({ todos, handleComplete }) => {
   };
 
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Todos</th>
-          <th>Date</th>
-          <th>Priority</th>
-          <th>Edit</th>
-          <th>Delete</th>
-        </tr>
-      </thead>
-      <tbody>
-        {todos?.map((data, index) => (
-          <tr
-            key={index}
-            onClick={() => strikethrough(index)}
-            style={
-              data.completed
-                ? { cursor: "pointer", textDecoration: "red line-through" }
-                : { cursor: "pointer" }
-            }
-          >
-            <td>{data.todo}</td>
-            <td>{data.datetime}</td>
-            <td>{data.priority}</td>
-            <td>
-              <button>
-                <i className="fa-sharp fa-solid fa-pen-to-square"></i>
-              </button>
-            </td>
-            <td>
-              <button>
-                <i className="fa-sharp fa-solid fa-trash"></i>
-              </button>
-            </td>
+    <div className="todos-table">
+      <table>
+        <thead>
+          <tr>
+            <th>Todos</th>
+            <th>Date</th>
+            <th>Priority</th>
+            <th>Edit</th>
+            <th>Delete</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {todos?.map((data, index) => (
+            <tr
+              key={index}
+              onClick={() => strikethrough(index)}
+              style={
+                data.completed
+                  ? { cursor: "pointer", textDecoration: "red line-through" }
+                  : { cursor: "pointer" }
+              }
+            >
+              <td>{data.todo}</td>
+              <td>{data.datetime}</td>
+              <td>{data.priority}</td>
+              <td>
+                <button>
+                  <i className="fa-sharp fa-solid fa-pen-to-square"></i>
+                </button>
+              </td>
+              <td>
+                <button>
+                  <i className="fa-sharp fa-solid fa-trash"></i>
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
