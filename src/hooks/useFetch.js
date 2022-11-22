9d1e8a13cfb867eb3caa1385caeada128103b3c7
sagar-aryal <sagarandaryal@gmail.com>
2022-11-22 04:39:40 +0200

    implementing useFetch costum hook

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,25 +1,26 @@
 import { useEffect, useState } from "react";
 
 const useFetch = (url) => {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState([]);
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
 
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch(url);
+      const data = await response.json();
+      setData(data);
+    } catch (error) {
+      setError(true);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setLoading(true);
-        const response = await fetch(url);
-        const data = await response.json();
-        setData(data);
-      } catch (error) {
-        setError(true);
-      } finally {
-        setLoading(false);
-      }
-    };
     fetchData();
-  }, [url]);
+  }, []);
 
   return { data, loading, error };
 };
